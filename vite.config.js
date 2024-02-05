144a814d09010ccff9518b000f7c07f476625668
R.V.SARISH <143780630+rvsarish@users.noreply.github.com>
2024-02-05 11:30:04 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+build: {
+  outDir: 'build'
+}
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
